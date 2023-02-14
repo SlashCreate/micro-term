661ccc5bbe7e0e94ce20a21e6046b99d282fab65
SlashCreate <112573796+SlashCreate@users.noreply.github.com>
2023-02-14 11:36:20 -0500

    Update microterm.py

--- microterm.py
+++ microterm.py
@@ -2,10 +2,27 @@ import sys
 import os
 
 global command
+global cmdlist
 command = {'help': ''}
+cmdlist = ['help']
 
-def cmd(name: str, code: str):
+def new_cmd(name: str, code: str):
   command[name] = code
+  cmdlist.append(name)
 
 def term(cmd: str):
   exec(command[cmd])
+
+def keep():
+  while True:
+    pass
+
+def run_sys(cmd: str):
+  os.system(cmd)
+  
+def input_cmd(prompt='> ', check=True, errmsg='No command', msg='Executing', spliter='//'):
+  try:
+    inp = input(prompt)
+    cmdlist.index(name)
+    print(msg)
+    term(inp.split())
